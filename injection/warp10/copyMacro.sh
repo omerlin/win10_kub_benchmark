d3e79f1f6d1d96a868aea8ad7b731ba736d1fd54
Morel Dominique <DOMOREL@gemalto.com>
2021-04-23 10:45:53 +0200

    macros OK

--- injection/warp10/copyMacro.sh
+++ injection/warp10/copyMacro.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 POD=$(kubectl -n warpdemo get pod -o custom-columns=:metadata.name)
 kubectl -n warpdemo exec -it $POD -- mkdir -p /opt/warp10/macros/Requeteur
+
 cat mod1_req.mc2 | kubectl -n warpdemo exec -it $POD -- tee /opt/warp10/macros/Requeteur/mod1_req.mc2
--- injection/warp10/testMacro.sh
+++ injection/warp10/testMacro.sh
@@ -1,12 +1,24 @@
-#!/bin/bash
-set -e
-bash checkEnv.sh
-if [ $# -ne 1 ] ; then
-  echo missing label A/B/C
-  exit
+vars="silent tag"
+silent=false
+
+for arg in $* ; do
+  argName=$(echo ${arg} | sed 's/--//g' | cut -f1 -d=)
+  argValue=$(echo $arg | sed 's/--//g' | cut -f2 -d=)
+  echo ${vars} | grep ${argName} >/dev/null && export ${argName}=${argValue}
+done
+
+if [ -z ${tag} ] ; then
+  echo missing tag A/B/C
+  exit 1
 fi
-if [ ! -f ./testMod${1}.mc2 ] ; then
-  echo ./testMod${1}.mc2 not found
-  exit
+if [ ! -f ./testMod${tag}.mc2 ] ; then
+  echo ./testMod${tag}.mc2 not found
+  exit 1
 fi
-sed "s/readToken/${READ_TOKEN}/g" ./testMod${1}.mc2 | curl -i -T - -H 'Transfer-Encoding: chunked' ${WARP_URL}/exec
+
+if ${silent} ; then
+  sed "s/readToken/${READ_TOKEN}/g" ./testMod${tag}.mc2 | curl -i -T - -H 'Transfer-Encoding: chunked' ${WARP_URL}/exec > /dev/null
+else
+  sed "s/readToken/${READ_TOKEN}/g" ./testMod${tag}.mc2 | curl -i -T - -H 'Transfer-Encoding: chunked' ${WARP_URL}/exec
+fi
+
