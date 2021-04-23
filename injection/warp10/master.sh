46f692b5969f9e38d1030d3db7b35d3ef52326f3
Morel Dominique <DOMOREL@gemalto.com>
2021-04-23 12:00:01 +0200

    master

--- injection/warp10/master.sh
+++ injection/warp10/master.sh
@@ -2,10 +2,10 @@
 set -e
 bash checkEnv.sh
 
-rm ${SYNC_FILE}
+rm -rf ${SYNC_FILE}
 (sleep 3600; touch ${SYNC_FILE}) &
 
-for i in $(seq 1 2); do
+for i in $(seq 1 5); do
   nohup bash measure.sh randomFetch.sh > fetch${i}.nohup & 
   nohup bash measure.sh randomMacro.sh > macro${i}.nohup & 
 done
