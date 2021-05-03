d00558dd2226cc0f3afcee6dc6c972124e0a36f7
Morel Dominique <DOMOREL@gemalto.com>
2021-05-03 19:09:18 +0200

    stats

--- injection/warp10/stats.sh
+++ injection/warp10/stats.sh
@@ -2,21 +2,30 @@
 set -e
 bash ./checkEnv.sh
 
-start=0
-stop=999999
+start="1970-01-01T00:00:00.000000Z"
+stop="2999-01-01T00:00:00.000000Z"
 
-start="1970-01-01T00:00:00.$(printf '%06d' $start)Z"
-stop="2999-01-01T00:00:00.$(printf '%06d' $stop)Z"
+total_cmd=0
+
+workFile=/tmp/tmp1$$
+statFile=/tmp/tmp2$$
+
+touch $statFile
 
 for cmd in fetchGTS.sh testMacro.sh ; do
   for tag in A B C ; do
-    curl -i -H X-Warp10-Token:${READ_TOKEN} "${WARP_URL}/fetch?start=${start}&stop=${stop}&selector=K8S_BENCH%7Bargs~.*tag%3A${tag}%2Ccmd%3D.%2F${cmd}%7D&format=fulltext&dedup=false" > /tmp/tmp1$$
-    echo Average ${cmd} ${tag} = $(cat /tmp/tmp1$$ | awk '{ total += $3 } END { print total/NR/1000 }') ms >> /tmp/tmp2$$
+    curl -s -H X-Warp10-Token:${READ_TOKEN} "${WARP_URL}/fetch?start=${start}&stop=${stop}&selector=K8S_BENCH%7Bargs~.*tag%3A${tag}%2Ccmd%3D.%2F${cmd}%7D&format=fulltext&dedup=false" > $workFile
+    if [ ! -s $workFile ] ; then
+      continue
+    fi
+    echo Average ${cmd} ${tag} = $(cat $workFile | awk '{ total += $3 } END { print total/NR/1000 }') ms >> $statFile
+    total_cmd=$((${total_cmd}+$(wc -l $workFile | cut -d' ' -f1)))
   done
 done
 
-cat /tmp/tmp2$$
+cat $statFile
+echo "Total commands ${total_cmd}"
 
-rm /tmp/tmp1$$
-rm /tmp/tmp2$$
+rm -f $workFile
+rm -f $statFile
 
